b3f7b2248284b2e53c50237918af7575b430113f
KrystianMank <man-kry-2005@wp.pl>
2023-11-30 13:30:17 +0100

    cos

---
+++ ecommerce/store1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def store(request):
+    context = {}
+    return render(request, 'store1/store.html', context)
+    
+def cart(request):
+    context = {}
+    return render(request, 'store1/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store1/checkout.html', context)
